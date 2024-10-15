a340294838bbcb6a1362b32065ab6db14bb2510a
Nick Nicker <nick4nicker@gmail.com>
2024-10-15 09:56:07 +0200

    pas_handler_v2_pwm: Experiment further with ALGORITHM_EQ_COMPONENT, set to 2.5

--- firmware/stm32/helpers/pas_handler_v2_pwm/Core/Inc/algorithm.h
+++ firmware/stm32/helpers/pas_handler_v2_pwm/Core/Inc/algorithm.h
@@ -11,7 +11,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define ALGORITHM_EQ_COMPONENT 	3.3
+#define ALGORITHM_EQ_COMPONENT 	2.5
 #define ALGORITHM_EQ_FACTOR		1.1
 
 void calculateDutyCycle(float x);
